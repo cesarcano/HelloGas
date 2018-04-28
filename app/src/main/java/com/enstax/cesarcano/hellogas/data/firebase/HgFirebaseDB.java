db288b618532b2ddbad46e58d60949b0718da6db
César Cano <cesarcanojmz@gmail.com>
2018-04-28 15:25:35 -0500

    Modulo de firebase

--- app/src/main/java/com/enstax/cesarcano/hellogas/data/firebase/HgFirebaseDB.java
+++ app/src/main/java/com/enstax/cesarcano/hellogas/data/firebase/HgFirebaseDB.java
@@ -1,6 +1,5 @@
 package com.enstax.cesarcano.hellogas.data.firebase;
 
-import com.enstax.cesarcano.hellogas.data.firebase.model.Gasolinera;
 import com.enstax.cesarcano.hellogas.data.firebase.model.User;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,10 +19,5 @@ public class HgFirebaseDB {
     public void createUser(String userId, String nombre, String email) {
         User user = new User(nombre, email);
         mDatabase.child("users").child(userId).setValue(user);
-    }
-
-    public void createGasStation(String id, String marca, String direccion, Double latitud, Double longitud) {
-        Gasolinera gasolinera = new Gasolinera(marca, direccion, latitud, longitud);
-        mDatabase.child("gasolineras").child(id).setValue(gasolinera);
     }
 }
